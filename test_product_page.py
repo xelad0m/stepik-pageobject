985ae791a24aa88827c83024f57bb38078a20989
xelad0m <m0dalex@yandex.ru>
2021-09-24 12:49:04 +0300

    parametrization reduced

--- test_product_page.py
+++ test_product_page.py
@@ -6,7 +6,7 @@ from pages.login_page import LoginPage
 from pages.basket_page import BasketPage
 
 product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-urls = [f"{product_base_link}/?promo=offer{n}" for n in range(10)]
+urls = [f"{product_base_link}/?promo=offer{n}" for n in range(6,9)]
 
 xfails = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", ]
 marked_urls = [url if url not in xfails else pytest.param(url, marks=pytest.mark.xfail) for url in urls]
@@ -60,6 +60,7 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser, 
     page.add_to_basket()
     page.should_not_be_success_message()
 
+
 @pytest.mark.parametrize('link', [product_base_link, ])
 def test_guest_cant_see_success_message(browser, link):
     page = ProductPage(browser, link)
@@ -75,11 +76,13 @@ def test_message_disappeared_after_adding_product_to_basket(browser, link):
     page.add_to_basket()
     page.should_disapear_success_message()
 
+
 @pytest.mark.parametrize('link', [product_base_link, ])
 def test_guest_should_see_login_link_on_product_page(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
+
 
 @pytest.mark.need_review
 @pytest.mark.parametrize('link', [product_base_link, ])
@@ -89,6 +92,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser, link):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()       # выполнение тестов на странице логина
+
 
 @pytest.mark.need_review
 @pytest.mark.parametrize('link', [product_base_link, ])
